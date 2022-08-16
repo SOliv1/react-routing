2b2d71876c1c8129053754231382cae7c4df5010
SOliv1 <s.oliver1@icloud.com>
2022-08-17 00:35:34 +0100

    add api to shop not working

--- src/Shop.js
+++ src/Shop.js
@@ -1,14 +1,31 @@
 
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import './App.css';
 
 function Shop() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    const data = await fetch(
+      'https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get'
+
+    );
+
+
+    const items = await data.json();
+    console.log(items);
+
+  };
+
   return (
-    <nav>
+      <nav>
         <h1>Shop Page</h1>
-    </nav>
+      </nav>
+
   );
+
 }
 
 export default Shop;
