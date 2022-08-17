247a977f21a034d220c2e0110ca9894e1bcbd2b3
SOliv1 <s.oliver1@icloud.com>
2022-08-17 18:23:59 +0100

    modify ItemDetail

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -3,28 +3,35 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 
-function Item() {
+function ItemDetail({ match }) {
     useEffect(() => {
         fetchItem();
+        console.log('match');
     }, []);
 
-     const [Item, setItem] = useState({});
+     const [item, setItem] = useState({
+       images: {}
+     });
 
      const fetchItem = async () => {
        const fetchItem = await fetch(
          'https://fortnite1.p.rapidapi.com/upcoming/get?id=${ match.params.id }'
 
       );
+      const item = await fetchItem.json();
+      setItem(item);
+      console.log(item);
 
-     }
+    };
 
   return (
     <div>
-      <h1>Item</h1>
+      <h1>{item.name}</h1>
+      <img src={item.images.transparent} alt="" />
     </div>
 
   );
 
 }
 
-export default Item;
+export default ItemDetail;
